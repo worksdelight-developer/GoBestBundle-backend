50b1a59185bcf9118d06c55955c4cb49dda330ea
Gobinda <123453000+gobindwd@users.noreply.github.com>
2024-04-17 17:14:27 +0530

    New Module For orderPlace250

--- app/Http/Controllers/AutomaticOrderController.php
+++ app/Http/Controllers/AutomaticOrderController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventory;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AutomaticOrderController extends Controller
 {
@@ -12,6 +15,13 @@ class AutomaticOrderController extends Controller
     public function orderPlace250(Request $request)
     {
 
-        dd('4567');
+        $users = User::select('UID')->get();
+        foreach ($users  as $user) {
+            $userCart = DB::table('cart')->where('user_email', $user->email)->get();
+            if (count($userCart) != 0) {
+                dd($userCart);
+            }
+        }
+        // dd($user, 'inventory');
     }
 }
